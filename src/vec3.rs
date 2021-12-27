1acb76739fffe5b6a5dfeaee869de6edfe9793f7
df@ <@>
2021-12-27 15:28:50 +0000

    wrote Vec3 Add trait

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,3 +1,5 @@
+use std::ops::*;
+#[derive(Debug, PartialEq)]
 struct Vec3 {
     e: [f64; 3],
 }
@@ -8,12 +10,33 @@ impl Vec3 {
     }
 }
 
+impl Add for Vec3 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(
+            self.e[0] + rhs.e[0],
+            self.e[1] + rhs.e[1],
+            self.e[2] + rhs.e[2],
+        )
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn Vec3_new() {
+    fn vec3_new() {
         let v = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(v.e[0], 1.0);
+        assert_eq!(v.e[1], 2.0);
+        assert_eq!(v.e[2], 3.0);
+    }
+
+    #[test]
+    fn vec3_add_vec3() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(11.0, 22.0, 33.0);
+        assert_eq!(v1 + v2, Vec3::new(12.0, 24.0, 36.0));
     }
 }
