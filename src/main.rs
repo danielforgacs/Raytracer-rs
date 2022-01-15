09b13830f085973ead3a7723aa1abee8ed37a50e
df@ <@>
2022-01-15 01:05:22 +0000

    started Vec3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod image;
+mod vec3;
 
 fn main() {
     let mut image = image::Image::new(256, 64, None);
---
+++ src/vec3.rs
@@ -0,0 +1,38 @@
+type CoordType = f32;
+
+struct Vec3 {
+    x: CoordType,
+    y: CoordType,
+    z: CoordType,
+}
+
+impl Vec3 {
+    fn new(x: CoordType, y: CoordType, z: CoordType) -> Self {
+        Self { x, y, z }
+    }
+
+    fn get_x(&self) -> CoordType {
+        self.x
+    }
+
+    fn get_y(&self) -> CoordType {
+        self.y
+    }
+
+    fn get_z(&self) -> CoordType {
+        self.z
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn vec3_constructor() {
+        let vec = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(vec.get_x(), 1.0);
+        assert_eq!(vec.get_y(), 2.0);
+        assert_eq!(vec.get_z(), 3.0);
+    }
+}
