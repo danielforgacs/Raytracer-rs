1096667444856274e56013d6db60066cece1592d
df@ <@>
2022-01-15 13:52:04 +0000

    fmt

--- src/ray.rs
+++ src/ray.rs
@@ -21,5 +21,7 @@ impl Ray {
 pub fn ray_colour(ray: &Ray) -> vec3::Colour {
     let unit_direction = vec3::unit_vector(&ray.get_direction());
     let target = 0.5 * (unit_direction.get_y() + 1.0);
-    (1.0 - target) * vec3::Colour::new(1.0, 1.0, 1.0) + target * vec3::Colour::new(0.5, 0.7, 1.0)
+    (1.0 - target)
+        * vec3::Colour::new(1.0, 1.0, 1.0)
+        + (target * vec3::Colour::new(0.5, 0.7, 1.0))
 }
