85a300428b58775a9d6047bfc4c971bddfb43287
df@ <@>
2022-01-15 22:01:02 +0000

    sphere w normals ok

--- src/ray.rs
+++ src/ray.rs
@@ -27,8 +27,20 @@ impl Ray {
 }
 
 pub fn ray_colour(ray: &Ray) -> vec3::Colour {
-    if hit_sphere(&vec3::Point3::new(0.0, 0.0, -1.0), 0.5, &ray) {
-        return vec3::Colour::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(
+        &vec3::Point3::new(0.0, 0.0, -1.0),
+        0.5,
+        &ray,
+    );
+    if t > 0.0 {
+        let Normal = vec3::unit_vector(
+            &(ray.at(t) - vec3::Vec3::new(0.0, 0.0, -1.0))
+        );
+        return 0.5 * vec3::Colour::new(
+            Normal.get_x() + 1.0,
+            Normal.get_y() + 1.0,
+            Normal.get_z() + 1.0,
+        );
     }
     let unit_direction = vec3::unit_vector(&ray.get_direction());
     let t = 0.5 * (unit_direction.get_y() + 1.0);
@@ -37,11 +49,15 @@ pub fn ray_colour(ray: &Ray) -> vec3::Colour {
         + (t * vec3::Colour::new(0.5, 0.7, 1.0))
 }
 
-pub fn hit_sphere(center: &vec3::Point3, radius: f64, ray: &Ray) -> bool {
+pub fn hit_sphere(center: &vec3::Point3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.get_origin() - *center;
     let a = vec3::dot(&ray.get_direction(), &ray.get_direction());
     let b = 2.0 * vec3::dot(&oc, &ray.get_direction());
     let c = vec3::dot(&oc, &oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
