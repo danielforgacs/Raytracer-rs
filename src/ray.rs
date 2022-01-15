562860632f304df4f28f28e615821b39586c6d01
df@ <@>
2022-01-15 19:56:56 +0000

    .

--- src/ray.rs
+++ src/ray.rs
@@ -16,12 +16,16 @@ impl Ray {
     fn get_direction(&self) -> vec3::Vec3 {
         self.direction
     }
+
+    fn at(&self, t: f64) -> vec3::Point3 {
+        self.origin + t * self.get_direction()
+    }
 }
 
 pub fn ray_colour(ray: &Ray) -> vec3::Colour {
     let unit_direction = vec3::unit_vector(&ray.get_direction());
-    let target = 0.5 * (unit_direction.get_y() + 1.0);
-    (1.0 - target)
+    let t = 0.5 * (unit_direction.get_y() + 1.0);
+    (1.0 - t)
         * vec3::Colour::new(1.0, 1.0, 1.0)
-        + (target * vec3::Colour::new(0.5, 0.7, 1.0))
+        + (t * vec3::Colour::new(0.5, 0.7, 1.0))
 }
