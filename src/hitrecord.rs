7f5e29a7ab4f040322647dbefd453a9fb5dee7b1
df@ <@>
2022-01-16 19:27:45 +0000

    _wip_

--- src/hitrecord.rs
+++ src/hitrecord.rs
@@ -5,7 +5,6 @@ struct HitRecord {
     p: Point3,
     normal: Vec3,
     t: f64,
-    is_front_face: bool,
 }
 
 impl HitRecord {
@@ -14,15 +13,18 @@ impl HitRecord {
             p: Point3::new(),
             normal: Vec3::new(),
             t: 1.0,
-            is_front_face: true,
         }
     }
 
-    fn set_face_normal(&self, ray: &Ray, outward_normal: Vec3) {
-        let front_face = dot(&ray.get_direction(), &outward_normal) < 0.0;
-        let normal = match front_face {
-            true => outward_normal,
-            false => -1.0 * outward_normal,
-        };
-    }
+    // fn set_face_normal(&self, ray: &Ray, outward_normal: Vec3) {
+    //     let front_face = dot(&ray.get_direction(), &outward_normal) < 0.0;
+    //     let normal = match front_face {
+    //         true => outward_normal,
+    //         false => -1.0 * outward_normal,
+    //     };
+    // }
+}
+
+trait Hittable {
+    fn hit(ray: &Ray, t_min: f64, t_max: f64, rec: &HitRecord) -> bool;
 }
